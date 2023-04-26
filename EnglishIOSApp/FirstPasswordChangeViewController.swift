be5c57f9f9c6d2e913c8018c1609366557827437
LucaSick <lucasnike1337@yandex.ru>
2023-04-27 00:38:20 +0300

    add profile screen

--- EnglishIOSApp/FirstPasswordChangeViewController.swift
+++ EnglishIOSApp/FirstPasswordChangeViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class FirstPasswordChangeViewController: UIViewController {
     
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func EditTextFields() {
         let bottomLine1 = CALayer()
         bottomLine1.frame = CGRectMake(0.0, emailTextField.frame.height - 1, emailTextField.frame.width, 1.0)
@@ -29,6 +35,77 @@ class FirstPasswordChangeViewController: UIViewController {
         ConfirmButton.layer.cornerRadius = ConfirmButton.frame.height / 2
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func ConfirmEmailForReset(_ sender: Any) {
+        if (!emailTextField.hasText) {
+            showAlert(title: "Ошибка во время регистрации", message: "Не введена почта")
+            return
+        }
+        
+        guard let url = URL(string: "http://localhost:8080/authApi/startChangePassword-mobile") else { return }
+        
+        struct Body: Codable {
+            let email: String
+        }
+        
+        let request_body = Body(email: emailTextField.text!)
+
+        guard let jsonData = try? JSONEncoder().encode(request_body) else {
+            print("Error: Trying to convert model to JSON data")
+            return
+        }
+        // Create the url request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type") // the request is JSON
+
+        request.httpBody = jsonData
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("Error: error calling POST")
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            if let httpresponse = response as? HTTPURLResponse {
+                print(httpresponse.statusCode)
+            }
+            do {
+                guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("Error: Cannot convert data to JSON object")
+                    return
+                }
+                let success = jsonObject["success"] as! Bool
+                print(success)
+                if (success == false) {
+                    let reason = jsonObject["reason"] as! String
+                    DispatchQueue.main.async {
+                        self.showAlert(title: "Ошибка во время смены пароля", message: reason)
+                        return
+                    }
+                }
+                guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
+                    print("Error: Cannot convert JSON object to Pretty JSON data")
+                    return
+                }
+                guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
+                    print("Error: Couldn't print JSON in String")
+                    return
+                }
+                
+                print(prettyPrintedJson)
+                DispatchQueue.main.async {
+                    let SecondPasswordChangeViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondPasswordChangeViewController") as! SecondPasswordChangeViewController
+                    self.present(SecondPasswordChangeViewController, animated:true, completion:nil)
+                }
+            } catch {
+                print("Error: Trying to convert JSON data to string")
+                return
+            }
+        }.resume()
     }
     
 
--- EnglishIOSApp/SecondPasswordChangeViewController.swift
+++ EnglishIOSApp/SecondPasswordChangeViewController.swift
@@ -40,8 +40,8 @@ class SecondPasswordChangeViewController: UIViewController {
     }
 
     @IBOutlet weak var CodeTextField: UITextField!
-    @IBOutlet weak var NewPassword2TextField: UITextField!
     @IBOutlet weak var NewPasswordTextField: UITextField!
+    @IBOutlet weak var NewPassword2TextField: UITextField!
     @IBOutlet weak var ChangePasswordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +50,103 @@ class SecondPasswordChangeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func ChangePassword(_ sender: Any) {
+        if (!CodeTextField.hasText) {
+            showAlert(title: "Ошибка во время смены пароля", message: "Не введен код")
+            return
+        }
+        
+        if (!NewPasswordTextField.hasText) {
+            showAlert(title: "Ошибка во время смены пароля", message: "Не введен новый пароль")
+            return
+        }
+        
+        if (!NewPassword2TextField.hasText) {
+            showAlert(title: "Ошибка во время смены пароля", message: "Не введено подтверждение нового пароля")
+            return
+        }
+        
+        if (NewPasswordTextField.text! != NewPassword2TextField.text!) {
+            showAlert(title: "Ошибка во время смены пароля", message: "Пароли не совпадают")
+            return
+        }
+        
+        guard let url = URL(string: "http://localhost:8080/authApi/restore-password/" + CodeTextField.text!) else { return }
+        
+        struct Body: Codable {
+            let newPassword: String
+        }
+        
+        let request_body = Body(newPassword: NewPasswordTextField.text!)
 
+        guard let jsonData = try? JSONEncoder().encode(request_body) else {
+            print("Error: Trying to convert model to JSON data")
+            return
+        }
+        // Create the url request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type") // the request is JSON
+
+        request.httpBody = jsonData
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print("Error: error calling POST")
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            if let httpresponse = response as? HTTPURLResponse {
+                print(httpresponse.statusCode)
+            }
+            do {
+                guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("Error: Cannot convert data to JSON object")
+                    return
+                }
+                let success = jsonObject["success"] as! Bool
+                print(success)
+                if (success == false) {
+//                    let reason = jsonObject["reason"] as! String
+                    DispatchQueue.main.async {
+                        self.showAlert(title: "Ошибка во время смены пароля", message: "Неверный код")
+                        return
+                    }
+                }
+                guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
+                    print("Error: Cannot convert JSON object to Pretty JSON data")
+                    return
+                }
+                guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
+                    print("Error: Couldn't print JSON in String")
+                    return
+                }
+                
+                print(prettyPrintedJson)
+                DispatchQueue.main.async {
+                    self.showAlert(title: "", message: "Пароль успешно изменён")
+                    return
+                }
+//                DispatchQueue.main.async {
+//                    let EmailConfirmationViewController = self.storyboard?.instantiateViewController(withIdentifier: "EmailConfirmationViewController") as! EmailConfirmationViewController
+//                    self.present(EmailConfirmationViewController, animated:true, completion:nil)
+//                }
+            } catch {
+                print("Error: Trying to convert JSON data to string")
+                return
+            }
+        }.resume()
+    }
+    
     /*
     // MARK: - Navigation
 
